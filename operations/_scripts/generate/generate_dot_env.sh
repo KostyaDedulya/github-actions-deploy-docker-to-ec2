4bde6b76f8039ee3f74a885c2731975396f7ad82
Kanstantsin Dziadziulia <eximal@gmail.com>
2023-05-19 14:02:02 +0200

    feat: add custom config

--- operations/_scripts/generate/generate_dot_env.sh
+++ operations/_scripts/generate/generate_dot_env.sh
@@ -7,3 +7,9 @@ echo "In generate_dot_env.sh"
 
 echo "$GHV_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/ghv.env"
 echo "$GHS_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/ghs.env"
+
+echo "$GQL_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/config/gql-gateway/production.env"
+echo "$AUTH_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/config/auth-subgraph/production.env"
+echo "$WALLET_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/config/wallet-subgraph/production.env"
+echo "$CHAT_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/config/chat-service/production.env"
+echo "$DB_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/config/db/production.env"
