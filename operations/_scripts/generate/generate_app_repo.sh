b20fb7c1d43c5de1b254ea5b129dafaf2d85bee9
Kanstantsin Dziadziulia <eximal@gmail.com>
2023-05-22 17:01:32 +0200

    fix: change env path

--- operations/_scripts/generate/generate_app_repo.sh
+++ operations/_scripts/generate/generate_app_repo.sh
@@ -9,17 +9,17 @@ GITHUB_REPO_NAME=$(echo $GITHUB_REPOSITORY | sed 's/^.*\///')
 echo "Copying files from GITHUB_WORKSPACE ($GITHUB_WORKSPACE) to ops repo's Ansible deployment (${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME})"
 mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
 
-mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/gql-gateway"
-mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/auth-subgraph"
-mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/wallet-subgraph"
-mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/chat-service"
-mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/db"
+mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/gql-gateway"
+mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/auth-subgraph"
+mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/wallet-subgraph"
+mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/chat-service"
+mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/db"
 
-echo "$GQL_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/gql-gateway/production.env"
-echo "$AUTH_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/auth-subgraph/production.env"
-echo "$WALLET_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/wallet-subgraph/production.env"
-echo "$CHAT_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/chat-service/production.env"
-echo "$DB_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/db/production.env"
+echo "$GQL_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/gql-gateway/production.env"
+echo "$AUTH_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/auth-subgraph/production.env"
+echo "$WALLET_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/wallet-subgraph/production.env"
+echo "$CHAT_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/chat-service/production.env"
+echo "$DB_ENV" > "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/config/env/db/production.env"
 
 TARGET_PATH="$GITHUB_WORKSPACE"
 if [ -n "$APP_DIRECTORY" ]; then
